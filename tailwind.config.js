f31861b97e13fa0876a0c59249c0f6fd5c90c252
Meeth <meeth2111@gmail.com>
2024-09-19 18:00:25 +0530

    ✅ Setup for theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,43 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slider: {
+          'from' : { transform: 'translateX(40px)' },
+          'to': { transform: 'translateX(-2188px)' },
+        },
+        slider_mobile: {
+          'from' : { transform: 'translateX(40px)' },
+          'to': { transform: 'translateX(-1935px)' },
+        },
+      },
+      animation: {
+        slider: 'slider 30s linear infinite',
+        slider_mobile: 'slider_mobile 30s linear infinite',
+      },
+
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        "lightBlue": "#D0E7FF",
+        "darkBlue": "#1F4E79",
+        "accentBlue": "#51A5E4",
+      },
+
+      screens: {
+        'xxl': '1680px',
+        'xxl2': '2800px',
+      },
+    },
   },
   plugins: [],
-}
+  variants: {
+    extend: {
+      animation: ['hover'],
+    },
+  },
+};
 
